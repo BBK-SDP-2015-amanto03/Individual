72c0e6d52b5e626bf0641d64b4993049ae766d42
Ahsan Manto <ahsanmanto@hotmail.com>
2015-02-11 18:59:22 +0000

    Initial Commit

---
+++ SimpleMachineLanguage/src/sml/OutInstruction.java
@@ -0,0 +1,37 @@
+package sml;
+
+/**
+ * This class ....
+ * 
+ * @author someone
+ */
+
+public class OutInstruction extends Instruction {
+
+	private int result;
+
+	public OutInstruction(String label, String op) {
+		super(label, op);
+	}
+
+	public OutInstruction(String label, int result) {
+		this(label, "out");
+		this.result = result;
+		
+		Global.resultOut = result;
+	}
+
+	@Override
+	public void execute(Machine m) {
+		//int value1 = m.getRegisters().getRegister(op1);
+		//int value2 = m.getRegisters().getRegister(op2);
+		//m.getRegisters().setRegister(result, value1 * value2);
+		//value1 = m.getRegisters().getRegister(result);
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + " from register " + Global.resultOut;
+
+	}
+}
